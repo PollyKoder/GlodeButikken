8ec73338c589be961ace49d54b7f48ab6433b4ed
TrymCoder <trymel.koder@gmail.com>
2024-01-10 14:07:48 +0100

    commit

--- KategorinoFunctionSite/produkter/Buttons.js
+++
@@ -1,5 +0,0 @@
-  // JavaScript function to redirect to the product page based on the clicked button
-  function redirectToProduct(productId) {
-    // Replace the following URL with the actual URL structure of your product pages
-    window.location.href = '/products/' + productId;
-}
