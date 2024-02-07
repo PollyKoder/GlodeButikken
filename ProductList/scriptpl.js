79b8ac8b874bf03d8ce982220dc91522aa867bb8
Sandra.B <105020067+Sandra@users.noreply.github.com>
2024-02-07 11:03:08 +0100

    javascript for dark and light mode

--- ProductList/scriptpl.js
+++ ProductList/scriptpl.js
@@ -25,11 +25,18 @@ function gridListToggle() {
 
 // Define your JavaScript functions here
 
-// Function to toggle dark mode
-function toggleMode() {
-    document.body.classList.toggle('dark-mode');
-}
+/* Light and Dark mode */
 
+function toggleMode() {
+    const body = document.body;
+    const modeToggle = document.querySelector('.mode-toggle');
+    const circle = document.querySelector('.circle');
+  
+    body.classList.toggle('dark-mode');
+    circle.classList.toggle('dark-mode');
+  
+    body.style.backgroundColor = body.classList.contains('dark-mode') ? '#222' : '#fff';
+  }
 // Function to toggle between grid and list view
 function toggleView() {
     const productList = document.querySelector('.divContainProducts');
