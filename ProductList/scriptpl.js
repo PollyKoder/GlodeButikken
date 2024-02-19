679c0a598f3e0e193811cf206d6ecf3a137da62b
Sandra.B <105020067+Sandra@users.noreply.github.com>
2024-02-19 14:52:01 +0100

    pictures and change on folder name new css and more html

--- ProductList/scriptpl.js
+++ ProductList/scriptpl.js
@@ -1,27 +1,3 @@
-let gridListButtons = document.querySelectorAll(".grid-list-button");
-
-gridListButtons.forEach((button) => {
-	button.addEventListener("click", gridListToggle);
-});
-
-function gridListToggle() {
-	let originalButton = this;
-
-	// Toggle the 'list' class to change the button's appearance
-	originalButton.classList.toggle("list");
-
-	// Toggle the icon based on the presence of the 'list' class
-	let dots = originalButton.querySelector(".dots");
-	let lines = originalButton.querySelector(".lines");
-	if (originalButton.classList.contains("list")) {
-		dots.style.display = "none";
-		lines.style.display = "block";
-	} else {
-		dots.style.display = "block";
-		lines.style.display = "none";
-	}
-}
-
 /* Light and Dark mode */
 function toggleMode() {
 	const body = document.body;
